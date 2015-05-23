418fd486c2a29bffce919d2266c5cfd51e335e88
Sean Oldham <seanosaur@me.com>
2015-05-22 21:56:27 -0500

    fix authentication error
    thanks to @paulstraw for that one!

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,5 +1,5 @@
 class EntriesController < ApplicationController
-  before_action :authenticate_user!, unless: [:show, :index]
+  before_action :authenticate_user!, except: [:show, :index]
 
   def new
     @entry = Entry.new
