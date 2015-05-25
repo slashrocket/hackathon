8d685d4b867bdb354166e8041d14828e5be9263a
Sean Oldham <seanosaur@me.com>
2015-05-25 00:45:39 -0500

    add slack logo for login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -233,7 +233,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :slack, ENV["API_KEY"], ENV["API_SECRET"], scope: 'identify, read', team: 'T0383B6CH'
+  config.omniauth :slack, ENV["API_KEY"], ENV["API_SECRET"], scope: 'identify, read', team: ENV["TEAM_ID"]
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
