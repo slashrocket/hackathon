a5760c835d475705ac2797f31bbddb5496179139
Daniel Climent <stokp1@gmail.com>
2015-05-25 11:53:53 +0300

    prevent users to manually go to /users/sign_up and sign up with anything other than slack

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   root 'main#home'
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, 
+      controllers: { omniauth_callbacks: "users/omniauth_callbacks" },
+      skip: [:sessions, :registrations]
+  devise_scope :user do
+    delete "users/sign_out(.:format)" => "devise/sessions#destroy", as: 'destroy_user_session'
+  end
   resources :entries
   resources :main
   get 'submissions', to: 'main#index'
-  get '/auth/slack/callback', to: 'sessions#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
