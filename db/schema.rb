4bf83482a1291e8b4d2f21f42052a8c12701a3df
Daniel James McMahon <danieljmcmahon@gmail.com>
2015-06-05 22:55:54 -0500

    Update schema for PostgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150524135324) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "entries", force: :cascade do |t|
     t.text     "name"
     t.text     "url"
@@ -22,7 +25,7 @@ ActiveRecord::Schema.define(version: 20150524135324) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "entries", ["user_id"], name: "index_entries_on_user_id"
+  add_index "entries", ["user_id"], name: "index_entries_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -44,8 +47,8 @@ ActiveRecord::Schema.define(version: 20150524135324) do
     t.string   "image"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["entries_id"], name: "index_users_on_entries_id"
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["entries_id"], name: "index_users_on_entries_id", using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
