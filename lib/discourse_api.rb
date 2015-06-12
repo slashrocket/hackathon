81f14665434196113e4f5b65e0f5b8bb8dc0a3d7
Daniel James McMahon <danieljmcmahon@gmail.com>
2015-06-11 23:02:11 -0500

    Refactor and clean up

--- lib/discourse_api.rb
+++ lib/discourse_api.rb
@@ -4,24 +4,23 @@ class DiscourseAPI
   API_KEY = ENV['DISCOURSE_API_KEY']
   API_USERNAME = ENV['DISCOURSE_API_USERNAME']
   DISCOURSE_URL = ENV['DISCOURSE_URL']
-  
+
   def initialize(username, badge_name)
     @username = username
     @badge_name = badge_name
   end
 
   def get_badge_id
-    url = "#{DISCOURSE_URL.to_s}/admin/badges.json?api_key=#{API_KEY.to_s}&api_username=#{API_USERNAME.to_s}"
+    url = "#{DISCOURSE_URL}/admin/badges.json?api_key=#{API_KEY}&api_username=#{API_USERNAME}"
     badges = HTTParty.get(url)
-    badge = badges['badges'].select{ |badge| badge['name'] == @badge_name  }.first
+    badge = badges['badges'].find { |badge| badge['name'] == @badge_name }
     badge['id']
   end
 
   def assign_badge
     badge_id = get_badge_id
-    params = {username: @username, badge_id: badge_id}
-    url = "#{DISCOURSE_URL.to_s}/user_badges?api_key=#{API_KEY.to_s}&api_username=#{API_USERNAME.to_s}"
+    params = { username: @username, badge_id: badge_id }
+    url = "#{DISCOURSE_URL}/user_badges?api_key=#{API_KEY}&api_username=#{API_USERNAME}"
     HTTParty.post(url, body: params)
   end
-
 end
