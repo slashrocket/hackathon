a2fd979649ec729399332dc4e1452704741a2ab1
Daniel Climent <stokp1@gmail.com>
2015-06-16 17:22:22 +0300

    create user in Discourse with slack's user data if it doesn't exist at the time of getting the badge assigned

--- lib/discourse_api.rb
+++ lib/discourse_api.rb
@@ -10,6 +10,25 @@ class DiscourseAPI
     @badge_name = badge_name
   end
 
+  def user_in_discourse?
+    url = "#{DISCOURSE_URL}/users/#{@username}.json"
+    response = HTTParty.get(url)
+    !response['user'].nil?
+  end
+
+  def create_user
+    user = User.find_by_username(@username)
+    url = "#{DISCOURSE_URL}/users?api_key=#{API_KEY}&api_username=#{API_USERNAME}"
+    params = {
+      name: user.username,
+      username: user.username,
+      email: user.email,
+      password: SecureRandom.hex,
+      active: true
+    }
+    HTTParty.post(url, body: params)
+  end
+
   def get_badge_id
     url = "#{DISCOURSE_URL}/admin/badges.json?api_key=#{API_KEY}&api_username=#{API_USERNAME}"
     badges = HTTParty.get(url)
@@ -18,6 +37,7 @@ class DiscourseAPI
   end
 
   def assign_badge
+    create_user unless user_in_discourse?
     badge_id = get_badge_id
     params = { username: @username, badge_id: badge_id }
     url = "#{DISCOURSE_URL}/user_badges?api_key=#{API_KEY}&api_username=#{API_USERNAME}"
