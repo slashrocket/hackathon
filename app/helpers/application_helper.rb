e6bf7c9b02305531b9dc978ea24e0e105ba787aa
Daniel Climent <stokp1@gmail.com>
2015-06-20 11:56:12 +0300

    link for admin panel

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def user_avatar(user)
     image_tag(user.image, size: '35x35', class: 'img-circle profile-image')
   end
+
+  def admin_button
+    link_to 'Admin Panel', admin_url, data: { no_turbolink: true }
+  end
 end
