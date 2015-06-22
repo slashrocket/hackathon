4c08d577bc808d0f2b327dd5309361b02e3fbd6c
Daniel Climent <stokp1@gmail.com>
2015-06-22 18:27:21 +0300

    prevent the initializer to run on rake tasks

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,3 +1,5 @@
-Setting.save_default(:sign_up, true)
-Setting.save_default(:submit_entry, false)
-Setting.save_default(:firebase_url, ENV['FIREBASEIO_URL'])
+unless ( File.basename($0) == 'rake')
+  Setting.save_default(:sign_up, true)
+  Setting.save_default(:submit_entry, false)
+  Setting.save_default(:firebase_url, ENV['FIREBASEIO_URL'])
+end
