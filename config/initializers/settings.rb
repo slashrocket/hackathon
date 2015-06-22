9443da48e1b760859712b16fbd1e2d985a01cc88
Daniel Climent <stokp1@gmail.com>
2015-06-22 18:30:29 +0300

    better way to evade initalizer run on rake db:migrate

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,4 +1,4 @@
-unless ( File.basename($0) == 'rake')
+if ActiveRecord::Base.connection.table_exists? 'settings'
   Setting.save_default(:sign_up, true)
   Setting.save_default(:submit_entry, false)
   Setting.save_default(:firebase_url, ENV['FIREBASEIO_URL'])
