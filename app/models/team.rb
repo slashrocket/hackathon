c2fd52eef6728043a6e10e8df30c91b9e6416cee
Daniel Climent <stokp1@gmail.com>
2015-06-29 12:52:47 +0300

    edit relationship to be hable to easily filter accepted users from non-accepted users

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,6 @@
 class Team < ActiveRecord::Base
   has_many :team_members
-  has_many :users, through: :team_members
+  has_many :users, -> { select("users.*, team_members.accepted AS team_members_accepted") },
+                   through: :team_members
   has_one :entry, as: :ownable
 end
