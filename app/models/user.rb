e1be28deeda71bf0dcacc41072c522f2edce48f6
Slacker87 <ccnp_pro@yahoo.com>
2015-06-29 05:00:13 +0000

    properly hide default devise login urls, also fix some more routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_one :entry, as: :ownable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: [:slack]
   belongs_to :team
