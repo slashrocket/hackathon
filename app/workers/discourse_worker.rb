f30c42400852374260a4c5cfb6aaee44fcf06d03
Daniel Climent <stokp1@gmail.com>
2015-06-30 18:16:42 +0300

    update discourse's worker

--- app/workers/discourse_worker.rb
+++ app/workers/discourse_worker.rb
@@ -1,6 +1,6 @@
 class DiscourseWorker
   include Sidekiq::Worker
-  def perform(username, badge_name, category)
-    DiscourseAPI.new(username, badge_name, category).assign_badge
+  def perform(team_id, badge_name, category)
+    DiscourseAPI.new(team_id, badge_name, category).assign_badge
   end
 end
