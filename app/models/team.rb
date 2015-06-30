783139b35268bd1dffd6d5405e3a47950ac80de4
Daniel Climent <stokp1@gmail.com>
2015-06-30 14:16:02 +0300

    delete TeamMembers when a Team is deleted

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,5 @@
 class Team < ActiveRecord::Base
-  has_many :team_members
+  has_many :team_members, dependent: :destroy
   has_many :users, -> { select("users.*, team_members.accepted AS team_members_accepted") },
                    through: :team_members
   has_one :entry
