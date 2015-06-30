9b49059503ff7f749280375254ebb33a872cd364
Daniel Climent <stokp1@gmail.com>
2015-06-30 14:16:36 +0300

    autoaccept owner into teammembers

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -1,4 +1,11 @@
 class TeamMember < ActiveRecord::Base
   belongs_to :user
   belongs_to :team
+  before_save :accept_owner
+
+  def accept_owner
+    if TeamMember.where(team_id: self.team_id).count < 1
+      self.accepted = true
+    end
+  end
 end
