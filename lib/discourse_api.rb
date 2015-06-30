303bbc518cde4cb6fe54d41ad6f9882f4fa6c048
Daniel Climent <stokp1@gmail.com>
2015-06-30 18:18:08 +0300

    redo part of the discourse api lib

--- lib/discourse_api.rb
+++ lib/discourse_api.rb
@@ -6,20 +6,20 @@ class DiscourseAPI
   API_USERNAME = ENV['DISCOURSE_API_USERNAME']
   DISCOURSE_URL = ENV['DISCOURSE_URL']
 
-  def initialize(username, badge_name, category)
-    @username = username
+  def initialize(team_id, badge_name, category)
+    @team_id = team_id
     @badge_name = badge_name
     @category = category
   end
 
-  def user_in_discourse?
-    url = "#{DISCOURSE_URL}/users/#{@username}.json"
+  def user_in_discourse?(user)
+    url = "#{DISCOURSE_URL}/users/#{user.username}.json"
     response = HTTParty.get(url)
     !response['user'].nil?
   end
 
-  def create_user
-    user = User.find_by_username(@username)
+  def create_user(user)
+    user = User.find_by_username(user.username)
     url = "#{DISCOURSE_URL}/users?api_key=#{API_KEY}&api_username=#{API_USERNAME}"
     params = {
       name: user.username,
@@ -38,18 +38,20 @@ class DiscourseAPI
     badge['id']
   end
 
-  def create_topic
-    url = "#{DISCOURSE_URL}/posts?api_key=#{API_KEY}&api_username=#{@username}"
-    user = User.includes(:entry, team: :entry).find_by_username(@username)
-    if user.entry
-      entry = user.entry
-      post = "##{entry.name}\n  Repository url: #{entry.url}\n  About the project:\n  #{entry.about}\n  "
-    elsif user.team.entry
-      entry = user.team.entry
-      user_list = user.team.users.collect{|u| "@#{u.username}"}.join('/n  ')
-      post = "##{entry.name}\n  Repository url: #{entry.url}\n  Team Members:\n  #{user_list}\n  About the project:\n  #{entry.about}\n  "
-    end
-    
+  def assign_badge(user)
+    create_user(user) unless user_in_discourse?(user)
+    badge_id = get_badge_id
+    params = { username: user.username, badge_id: badge_id }
+    url = "#{DISCOURSE_URL}/user_badges?api_key=#{API_KEY}&api_username=#{API_USERNAME}"
+    HTTParty.post(url, body: params)
+  end
+
+  def create_topic(team)
+    owner = team.owner
+    url = "#{DISCOURSE_URL}/posts?api_key=#{API_KEY}&api_username=#{owner.username}"
+    entry = team.entry
+    user_list = team.users.collect{|u| "@#{u.username}"}.join('/n  ')
+    post = "##{entry.name}\n  Project url: #{entry.url}\n  Team Members:\n  #{user_list}\n  About the project:\n  #{entry.about}\n  "
     html = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(:hard_wrap => true), autolink: true).render(post)
     params = {
       title: entry.name,
@@ -59,12 +61,15 @@ class DiscourseAPI
     HTTParty.post(url, body: params)
   end
 
-  def assign_badge
-    create_user unless user_in_discourse?
-    create_topic
-    badge_id = get_badge_id
-    params = { username: @username, badge_id: badge_id }
-    url = "#{DISCOURSE_URL}/user_badges?api_key=#{API_KEY}&api_username=#{API_USERNAME}"
-    HTTParty.post(url, body: params)
+  def post_entry
+    team = Team.includes(:owner, team_members:[:user]).find(@team_id)
+    team.users.each do |user|
+      assign_badge(user)
+    end
+    create_topic(team)
+  end
+
+  def update_topic
+    
   end
 end
